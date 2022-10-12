2ebfeb85d7cc9b7f1c46c28a971360b8c6b87640
Francisco Moncada B <fmoncadabuelvas0112@gmail.com>
2022-10-12 18:24:39 -0500

    [add] slider bottom's, input footer, input check

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,49 +1,85 @@
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import 'swiper/scss';
-import 'swiper/scss/navigation';
-import 'swiper/scss/pagination';
-
+import "swiper/scss";
+import "swiper/scss/navigation";
+import "swiper/scss/pagination";
+import "swiper/css/thumbs";
+import "swiper/css/free-mode";
 
 import "../assets/scss/main.scss";
+import { FreeMode, Navigation, Thumbs } from "swiper";
+
 
 import { Pagination } from "swiper";
 
 function Carousel() {
+  
   return (
-    <div className="relative z-0">
-      <p className="absolute z-10 text-white mt-24 text-2xl ml-5">
-          Gold Big Hoops </p>
-          <br />
-      <p className="absolute z-10 text-white text-xs mt-28 ml-5">
-          $ 68.00
-        </p>
-      <button className="absolute z-10 rounded-md border-solid border-l-2 border-r-2 border-x-2 border-y-2 text-white text-center mt-36 ml-5 p-2"> View Product</button>
+    <>
+    <div>
       <Swiper
-        spaceBetween={20}
-        pagination={{
-          clickable: true,
-        }}
-        modules={[Pagination]}
-        className="mySwiper p-3"
-      >
-        <SwiperSlide className="z-10">
-          <img src='./images/img1.png' alt='imagen1' className="rounded-lg w-full h-{299px} sm:h-80 sm:w-72" />
-        </SwiperSlide>
-        <SwiperSlide className="z-10">
-          <img src='./images/img2.jpg' alt='imagen2' className="rounded-lg w-full h-{299px} sm:h-80 sm:w-72" />
-        </SwiperSlide>
-        <SwiperSlide className="z-10">
-          <img src='./images/img3.jpg' alt='imagen3' className="rounded-lg w-full h-{299px} sm:h-80 sm:w-72" />
-        </SwiperSlide>
-        <SwiperSlide className="z-10">
-          <img src='./images/img4.jpg' alt='imagen4' className="rounded-lg w-full h-{299px} sm:h-80 sm:w-72" />
-        </SwiperSlide>
-        <SwiperSlide className="z-10">
-          <img src='./images/img5.jpg' alt='imagen5' className="rounded-lg w-full h-{299px} sm:h-80 sm:w-72" />
-        </SwiperSlide>
-      </Swiper>
-    </div>
+          
+          loop={true}
+          spaceBetween={-15}
+          slidesPerView={2}
+          freeMode={false}
+          watchSlidesProgress={true}
+          modules={[FreeMode, Navigation, Thumbs]}
+          className="mySwiper"
+        >
+          <SwiperSlide>
+            <button className="border-solid border-2 border-light-gray rounded-md h-12 w-40">
+              <p>Earring</p> 
+            </button>
+          </SwiperSlide>
+          <SwiperSlide>
+            <button className="border-solid border-2 border-light-gray rounded-md h-12 w-40">
+              <p>Necklace</p>  
+            </button>
+          </SwiperSlide>
+          <SwiperSlide>
+            <button className="border-solid border-2 border-light-gray rounded-md h-12 w-40" >
+              <p>Others</p>  
+            </button>
+          </SwiperSlide>
+        </Swiper>
+      </div>
+      <div className="relative z-0">
+        <p className="absolute z-10 text-white mt-44 text-2xl ml-5">
+          Gold Big Hoops{" "}
+        </p>
+        <br />
+        <p className="absolute z-10 text-white text-xs mt-48 ml-5">$ 68.00</p>
+        <button className="absolute z-10 rounded-md border-solid border-l-2 border-r-2 border-x-2 border-y-2 text-white text-center mt-56 ml-5 p-2">
+          {" "}
+          View Product
+        </button>
+        <Swiper
+          spaceBetween={20}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[Pagination]}
+          className="swiper p-3"
+        >
+          <SwiperSlide className="z-10">
+            <img src="./images/img1.png" alt="imagen1" className="rounded-lg" />
+          </SwiperSlide>
+          <SwiperSlide className="z-10">
+            <img src="./images/img2.jpg" alt="imagen2" className="rounded-lg" />
+          </SwiperSlide>
+          <SwiperSlide className="z-10">
+            <img src="./images/img3.jpg" alt="imagen3" className="rounded-lg" />
+          </SwiperSlide>
+          <SwiperSlide className="z-10">
+            <img src="./images/img4.jpg" alt="imagen4" className="rounded-lg" />
+          </SwiperSlide>
+          <SwiperSlide className="z-10">
+            <img src="./images/img5.jpg" alt="imagen5" className="rounded-lg" />
+          </SwiperSlide>
+        </Swiper>
+      </div>
+    </>
   );
 }
 
--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -21,7 +21,7 @@ function Products() {
                     alt="product1"
                />
                <div className="my-1">
-                    <p>Lira Earrings</p>
+                    <p className=" font-medium">Lira Earrings</p>
                     <p className="text-gold my-1"> $ 20,00</p>
                </div>
           </div>
@@ -32,20 +32,23 @@ function Products() {
                     alt="product2"
                />
                <div className="my-1">
-                    <p>Ollie Earrings</p>
+                    <p className=" font-medium">Ollie Earrings</p>
                     <p className="text-gold my-1"> $ 30,00</p>
                </div>
           </div>
-          <div className="p-2 rounded-md col-span-2 z-0">
-               <img 
-                    src={product3}
-                    className="h-30 w-full object-cover"
-                    alt="product3"
-               />
-               <div className="my-1 z-10">
-                    <p>Hal Earrings</p>
+          <div className="relative p-2 rounded-md col-span-2 z-0">
+               <div className="absolute z-10 mt-2 ml-1.5">
+               <span className="bg-gold text-white text-xs mr-2 px-2.5 py-1.5 rounded ">Earring</span>
+               </div>
+               <div className="absolute my-1 z-10 mt-28 ml-2">
+                    <p className=" font-medium">Hal Earrings</p>
                     <p className="text-gold my-1"> $ 23,00</p>
                </div>
+               <img 
+                    src={product3}
+                    className="z-0 h-30 w-full object-cover"
+                    alt="product3"
+               />
           </div>
           <div className="p-2 rounded-md">
                <img 
@@ -54,7 +57,7 @@ function Products() {
                     alt="product4" 
                />
                <div className="my-1">
-                    <p>Kaede Hair Pin</p>
+                    <p className=" font-medium">Kaede Hair Pin</p>
                     <p className="text-gold my-1"> $ 30,00</p>
                </div>
           </div>
@@ -65,7 +68,7 @@ function Products() {
                     alt="product5"
                />
                <div className="my-1">
-                    <p>Yuki Hair Pin</p>
+                    <p className=" font-medium">Yuki Hair Pin</p>
                     <p className="text-gold my-1"> $ 29,00</p>
                </div>
           </div>
