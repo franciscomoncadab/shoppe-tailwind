b3429c667f58cb3badd7150a92487cf591af829a
Francisco Moncada B <fmoncadabuelvas0112@gmail.com>
2022-11-23 00:13:21 -0500

    fix mobile menu

--- src/pages/DetailsProducts.tsx
+++ src/pages/DetailsProducts.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import SimilarItems from "../components/atoms/SimilarItems";
 import DetailsImage from "../components/organisms/DetailsImage";
 import Tabs from "../components/organisms/Tabs";
+import { imgProduct } from "../components/productImg";
 
 function DetailsProducts() {
   return (
     <div>
       <hr  className="hidden sm:block sm:mb-[128px] sm:mt-[12px] text-gray" />
-      <DetailsImage />
+      <DetailsImage 
+        // DetailsImageProps={imgProduct}
+      />
       <div>
         <Tabs />
       </div>
